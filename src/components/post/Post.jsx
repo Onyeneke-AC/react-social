1fd61386176552eb864efdf7c918d926d7d39f8e
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-04 09:33:14 +0100

    post component implementation

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,3 +1,4 @@
+import { MoreVert } from '@mui/icons-material'
 import './post.css'
 
 export default function post() {
@@ -6,17 +7,37 @@ export default function post() {
       <div className="postWrapper">
         <div className="postTop">
             <div className="postTopLeft">
-
+                <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
+                <span className="postUsername">
+                    Onyeneke Anthony
+                </span>
+                <span className="postDate">
+                    5 mins ago
+                </span>
             </div>
             <div className="postTopRight">
-
+                <MoreVert />
             </div>
         </div>
         <div className="postCenter">
-
+            <span className="postText">
+                Heyyy it's my first post
+            </span>
+            <img src="assets/post/1.jpeg" alt="" className='postImg' />
         </div>
         <div className="postBottom">
-            
+            <div className="postBottomLeft">
+                <img src="assets/like.png" alt="" className='likeIcon' />
+                <img src="assets/heart.png" alt="" className='likeIcon' />
+                <span className="postLikeCounter">
+                    32 people liked it
+                </span>
+            </div>
+            <div className="postBottomRight">
+                <span className="postCommentText">
+                    9 comments
+                </span>
+            </div>
         </div>
       </div>
     </div>
