7550dd5e03ff0c3ac71fb2555c94f4f6479919e3
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-05 10:34:50 +0100

    login page implementation

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,10 +3,11 @@ import { Users } from '../../dummyData'
 import Online from '../online/Online'
 
 export default function Rightbar({profile}) {
-  return (
-    <div className='rightbar'>
-        <div className="rightbarWrapper">
-          <div className="birthdayContainer">
+
+  const HomeRightBar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
             <img src="assets/gift.png" alt="" className="birthdayImg" />
             <span className="birthdayText">
               <b>Ogochukwu Madu</b> and  <b>3 other friends</b> have their birthdays today!
@@ -21,6 +22,51 @@ export default function Rightbar({profile}) {
               <Online key={u.id} user={u} />
             ))}
           </ul>
+      </>
+    )
+  }
+
+  const ProfileRightBar = () => {
+    return (
+    <>
+      <h4 className="rightbarTitle">User Information</h4>
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">Agbara</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">Anambra</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship:</span>
+          <span className="rightbarInfoValue">Very much single lol</span>
+        </div>
+      </div>
+      <h4 className="rightbarTitle">User Friends</h4>
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">
+            Ogochukwu Madu
+          </span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">
+            Ogochukwu Madu
+          </span>
+        </div>
+      </div>
+    </>
+    )
+  }
+
+  return (
+    <div className='rightbar'>
+        <div className="rightbarWrapper">
+          <ProfileRightBar />
         </div>
     </div>
   )
