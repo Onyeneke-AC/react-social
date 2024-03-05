5f520cfb23e2a4213f6164e70d37aa6848cc37e6
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-03-05 09:26:12 +0100

    profile page implementation

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,7 +2,7 @@ import React from './rightbar.css'
 import { Users } from '../../dummyData'
 import Online from '../online/Online'
 
-export default function Rightbar() {
+export default function Rightbar({profile}) {
   return (
     <div className='rightbar'>
         <div className="rightbarWrapper">
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -12,11 +12,20 @@ export default function Profile() {
         <Sidebar />
         <div className="profileRight">
             <div className="profileRightTop">
-
+                <div className="profileCover">
+                    <img src="assets/post/3.jpeg" alt="" className="profileCoverImg" />
+                    <img src="assets/person/7.jpeg" alt="" className="profileUserImg" />
+                </div>
+                <div className="profileInfo">
+                    <h4 className="profileInfoName">Onyeneke Anthony</h4>
+                    <span className="profileInfoDesc">
+                        Hello my friends!!!
+                    </span>
+                </div>
             </div>
             <div className="profileRightBottom">
                 <Feed />
-                <Rightbar />
+                <Rightbar profile/>
             </div>
         </div>
       </div>
